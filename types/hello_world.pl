8c5c63ee716164fa114b3fd7457d84cc2b5bd64b
Henry <th.saravana@gmail.com>
2018-09-24 20:06:14 +0530

    Initial hello world commit

---
+++ types/hello_world.pl
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+
+print 'Hello World';
+print "\nHello World\n";
+print "Hello " . "World\n";
+
+my $hello = 'Hello';
+my $world = "World";
+
+print "$hello $world\n";
+print $hello . " " . $world;
