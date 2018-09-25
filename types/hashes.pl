40b78a82b2ab6e8ed4b535480d3d79f037435fbb
Henry <th.saravana@gmail.com>
2018-09-25 07:41:55 +0530

    Hashes basics

---
+++ types/hashes.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+my %numbers = (
+    "one", 1,
+    "two", 2,
+    "three", 3
+);
+
+print $numbers{'one'};
+
+for my $number (keys %numbers) {
+    print "$number is $numbers{$number} \n";
+}
+
+for my $number (values %numbers) {
+    print "Just values - $number \n";
+}
+
+
+#Assigning
+$numbers{"four"} = 4;
+
+%numbers = (%numbers, "five" => 5, "six" => 6);
+
+print %numbers
